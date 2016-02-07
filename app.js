74909af5fbfed5bcce80a3aaf3d59a75cbf8fc4a
Juin Chiu <davidjuin0519@gmail.com>
2016-02-08 00:05:46 +0800

    add cronjob

--- app.js
+++ app.js
@@ -10,6 +10,35 @@ var users = require('./routes/users');
 
 var app = express();
 
+var file_name = 'test';
+var fs = require('fs');
+
+// set scheduler, download data and transfer to json
+var exec = require('child_process').exec;
+var cmd = 'wget http://data.taipei/youbike -O ' + file_name + '.gz && gunzip ' + file_name + '.gz -f';
+
+var CronJob = require('cron').CronJob;
+new CronJob('1 * * * * *', function() {
+  // console.log('You will see this message every minute');
+  exec(cmd, function(error, stdout, stderr) {
+    console.log("Run the command");
+    // command output is in stdout
+    // console.log(stdout);
+  });
+}, null, true, 'America/Los_Angeles');
+
+var youbike_data = {};
+if (fs.existsSync(file_name)) {
+  fs.readFile(file_name, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    youbike_data = JSON.parse(data);
+    console.log(youbike_data);
+    // eval(pry.it)
+  });
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
